bf180ee0e39046d037b4d0b1fde2b7da7c6f5b78
Alex Kostenko <0984181760ss@gmail.com>
2020-04-13 15:14:41 +0300

    I'm starting to develop

---
+++ TimePicker/index.tsx
@@ -0,0 +1,150 @@
+import React, { useState, useEffect } from 'react';
+import { withRouter, RouteComponentProps } from 'react-router';
+import { CaretUpOutlined, CaretDownOutlined } from '@ant-design/icons';
+import classNames from 'classnames';
+
+import useStyles from './style.jsx';
+
+interface TimeType {
+  h: number;
+  m: number;
+  meridiem: string;
+}
+
+interface TimePropsType {
+  value: string;
+  onChange: Function;
+}
+
+const CustomTimePicker: React.FC<TimePropsType & RouteComponentProps> = (props: any) => {
+  const { onChange, value } = props;
+  const [open, setOpen] = useState<boolean>(false);
+  const [time, setTime] = useState<TimeType>({
+    h: 12,
+    m: 0,
+    meridiem: 'AM',
+  });
+
+  useEffect(() => {
+    onChange(`${time.h}:${renderMibutes(time.m)} ${time.meridiem}`);
+  }, [time]);
+
+  useEffect(() => {
+    if (value) {
+      const h = parseInt(value.slice(0, 2));
+      const m = parseInt(value.slice(3).substr(0, 2));
+      const meridiem = value.slice(-2);
+
+      setTime({
+        h: h,
+        m: m,
+        meridiem: meridiem,
+      });
+    }
+  }, [value]);
+
+  const renderMibutes = (value: number) => (value.toString().length === 1 ? `0${value}` : value);
+
+  const changeHours = (value: number, direction: string) => {
+    let newValue = 0;
+    if (direction === 'up') {
+      newValue = value + 1;
+      if (newValue <= 12) {
+        return newValue;
+      }
+      return 1;
+    } else {
+      newValue = value - 1;
+      if (newValue >= 1) {
+        return newValue;
+      }
+      return 12;
+    }
+  };
+
+  const changeMinutes = (value: number, direction: string) => {
+    let newValue = 0;
+    if (direction === 'up') {
+      newValue = value + 5;
+      if (newValue <= 55) {
+        return newValue;
+      }
+      return 0;
+    } else {
+      newValue = value - 5;
+      if (newValue >= 0) {
+        return newValue;
+      }
+      return 55;
+    }
+  };
+
+  const changeMeridiem = (value: string) => {
+    if (value === 'PM') {
+      return 'AM';
+    }
+    return 'PM';
+  };
+
+  const changeTime = (typeDate: string, direction: string) => {
+    switch (typeDate) {
+      case 'h':
+        setTime(prev => ({
+          ...prev,
+          h: changeHours(prev.h, direction),
+        }));
+        break;
+
+      case 'm':
+        setTime(prev => ({
+          ...prev,
+          m: changeMinutes(prev.m, direction),
+        }));
+        break;
+
+      case 'meridiem':
+        setTime(prev => ({
+          ...prev,
+          meridiem: changeMeridiem(prev.meridiem),
+        }));
+        break;
+
+      default:
+        alert('error');
+        break;
+    }
+  };
+
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <div onClick={() => setOpen(!open)}>
+        {`${time.h}:${renderMibutes(time.m)} ${time.meridiem}`}
+      </div>
+      <div className={classNames(open ? classes.tooltip : classes.hidden)}>
+        <div className={classes.angle}> </div>
+        <div className={classes.col}>
+          <CaretUpOutlined className={classes.icon} onClick={() => changeTime('h', 'up')} />
+          {time.h}
+          <CaretDownOutlined className={classes.icon} onClick={() => changeTime('h', 'down')} />
+        </div>
+        <div className={classes.col}>
+          <CaretUpOutlined className={classes.icon} onClick={() => changeTime('m', 'up')} />
+          {renderMibutes(time.m)}
+          <CaretDownOutlined className={classes.icon} onClick={() => changeTime('m', 'down')} />
+        </div>
+        <div className={classes.col}>
+          <CaretUpOutlined className={classes.icon} onClick={() => changeTime('meridiem', 'up')} />
+          {time.meridiem}
+          <CaretDownOutlined
+            className={classes.icon}
+            onClick={() => changeTime('meridiem', 'down')}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default withRouter(CustomTimePicker);
---
+++ TimePicker/style.jsx
@@ -0,0 +1,64 @@
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    background: 'transparent',
+    position: 'relative',
+    cursor: 'pointer',
+    color: '#4D4D4D',
+  },
+  tooltip: {
+    position: 'absolute',
+    top: '35px',
+    left: '-23px',
+    display: 'flex',
+    padding: '12px',
+    background: '#FFFFFF',
+    borderRadius: '10px',
+    boxShadow: '0px 0px 6px rgba(0, 0, 0, 0.1)',
+    zIndex: '1'
+  },
+  hidden: {
+    display: 'none'
+  },
+  col: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    fontSize: '18px',
+    transition: '5s',
+
+    '& svg:hover': {
+      transform: 'scale(1.3)'
+    },
+    '&:not(:last-child)': {
+      marginRight: '15px',
+    },
+    '&:last-child': {
+      minWidth: '30px'
+    }
+  },
+  icon: {
+    color: '#1A7EE3',
+    fontSize: '20px',
+
+    '&:not(:last-child)': {
+      marginBottom: '5px'
+    },
+    '&:not(:first-child)': {
+      marginTop: '5px'
+    }
+  },
+  angle: {
+    position: 'absolute',
+    top: '-8px',
+    left: '43%',
+    width: '15px',
+    height: '15px',
+    background: ' #fff',
+    transform: 'rotate(45deg)',
+    boxShadow: '-4px -4px 6px rgba(0, 0, 0, 0.1)',
+  }
+}));
+
+export default useStyles;
